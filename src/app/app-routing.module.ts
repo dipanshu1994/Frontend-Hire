664d9a9ad99198bc1dfd942253a6fb7f14d7f8bb
Dipanshu <dipanshu@Mac155s-iMac.local>
2020-02-17 10:38:12 +0530

    Add Images and Styles.css

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DemoComponent } from './components/index/demo/demo.component'
+import { SignupComponent } from './components/index/signup/signup.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'demo', component: DemoComponent },
+  { path: 'signup', component: SignupComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SignupComponent } from './components/index/signup/signup.component';
+import { DemoComponent } from './components/index/demo/demo.component';
+import { HeaderComponent } from './components/index/header/header.component';
+import { FooterComponent } from './components/index/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignupComponent,
+    DemoComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
